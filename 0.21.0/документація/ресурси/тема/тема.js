097bfb80160b1d1ee9e26c3adca785330debe2f6
kohutd <davyd.kohut@gmail.com>
2024-09-18 20:04:47 +0000

    🧬

--- 0.21.0/документація/ресурси/тема/тема.js
+++ 0.21.0/документація/ресурси/тема/тема.js
@@ -75,25 +75,25 @@ if (!localStorage.getItem("tsil_ukr_light_mode")) {
     localStorage.setItem("tsil_ukr_light_mode", "auto");
 }
 
-document.querySelector(".XDocsPageNavigationFooter").innerHTML += `<button class="XDocsPageNavigationFooterConfigButton">
-  <svg class="no-dark-shadow" data-navigation-light-toggle-badge-auto="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M440-200q52 0 99-21t81-60q-128-8-214-99.5T320-600q0-13 1-25.5t3-24.5q-57 32-90.5 88T200-440q0 100 70 170t170 70Zm0 80q-134 0-227-93t-93-227q0-134 93-227t227-93q5 0 10 .5t10 .5q-29 32-44.5 73T400-600q0 100 70 170t170 70q31 0 60.5-7.5T756-390q-18 118-108 194t-208 76Zm112-400 128-360h80l128 360h-76l-28-80H656l-28 80h-76Zm122-134h92l-46-146-46 146ZM407-381Z" /></svg>
-  <svg class="no-dark-shadow" data-navigation-light-toggle-badge-yes="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" /></svg>
-  <svg class="no-dark-shadow" data-navigation-light-toggle-badge-no="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" /></svg>
-</button>`;
+// document.querySelector(".XDocsPageNavigationFooter").innerHTML += `<button class="XDocsPageNavigationFooterConfigButton">
+//   <svg class="no-dark-shadow" data-navigation-light-toggle-badge-auto="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M440-200q52 0 99-21t81-60q-128-8-214-99.5T320-600q0-13 1-25.5t3-24.5q-57 32-90.5 88T200-440q0 100 70 170t170 70Zm0 80q-134 0-227-93t-93-227q0-134 93-227t227-93q5 0 10 .5t10 .5q-29 32-44.5 73T400-600q0 100 70 170t170 70q31 0 60.5-7.5T756-390q-18 118-108 194t-208 76Zm112-400 128-360h80l128 360h-76l-28-80H656l-28 80h-76Zm122-134h92l-46-146-46 146ZM407-381Z" /></svg>
+//   <svg class="no-dark-shadow" data-navigation-light-toggle-badge-yes="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" /></svg>
+//   <svg class="no-dark-shadow" data-navigation-light-toggle-badge-no="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" /></svg>
+// </button>`;
 
-checkDarkMode();
+// checkDarkMode();
 
-document.querySelector(".XDocsPageNavigationFooterConfigButton").addEventListener("click", () => {
-    const currentMode = localStorage.getItem("tsil_ukr_light_mode");
-    if (currentMode === "auto") {
-        localStorage.setItem("tsil_ukr_light_mode", "light");
-    } else if (currentMode === "light") {
-        localStorage.setItem("tsil_ukr_light_mode", "dark");
-    } else {
-        localStorage.setItem("tsil_ukr_light_mode", "auto");
-    }
-    checkDarkMode();
-});
+// document.querySelector(".XDocsPageNavigationFooterConfigButton").addEventListener("click", () => {
+//     const currentMode = localStorage.getItem("tsil_ukr_light_mode");
+//     if (currentMode === "auto") {
+//         localStorage.setItem("tsil_ukr_light_mode", "light");
+//     } else if (currentMode === "light") {
+//         localStorage.setItem("tsil_ukr_light_mode", "dark");
+//     } else {
+//         localStorage.setItem("tsil_ukr_light_mode", "auto");
+//     }
+//     checkDarkMode();
+// });
 
 document.querySelector(".XDocsPageNavigationSearch button").addEventListener("click", () => {
     const $searchInput = document.createElement("input");
