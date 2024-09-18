0e5703cc3d91a25e10266d42859858818c9fcab4
Davyd Kohut <davyd.kohut@gmail.com>
2024-09-18 15:17:45 +0300

    🧬

---
+++ ресурси/ціль.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.getElementById("UiCodeBlock").style.animation =
+            "spin 2s cubic-bezier(0.4, 0.2, 0.2, 1)";
+    }, 250);
+});
