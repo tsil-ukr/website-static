b07b6aa1b618eb0dd37a6b114396ef0e9ebe2cc4
kohutd <davyd.kohut@gmail.com>
2025-12-21 01:56:33 +0000

    🧬

--- ресурси/ціль.js
+++ ресурси/ціль.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    document.getElementById("UiCodeBlock").style.animation =
-      "spin 2s cubic-bezier(0.4, 0.2, 0.2, 1)";
+    document.documentElement.classList.add("readyToAnimate");
+
+    setTimeout(() => {
+      document.documentElement.classList.add("animationsDone");
+    }, 500);
   }, 250);
 });
 
